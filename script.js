790abfe9e98c40cb6d2372b22fd1b148c68dd913
Gautam <devgautam1231@gmail.com>
2021-04-06 16:26:16 +0530

    Wish your friend!

---
+++ script.js
@@ -0,0 +1,95 @@
+const circleUp = document.querySelector(".circleUp");
+const circleDown = document.querySelector(".circleDown");
+const html = document.querySelector("html");
+const button = document.querySelector(".btn");
+const text = document.querySelector(".text");
+const audio = new Audio("hb.mp3");
+
+let timer;
+let audioTimer;
+let count = -1;
+let promp = "";
+
+button.addEventListener("click", () => {
+  if (count === -1) {
+    promp = prompt("Enter your name to proceed.");
+
+    if (promp === null) {
+      alert("Please enter your name to proceed.");
+    } else {
+      count = 0;
+    }
+  }
+
+  if (promp !== null) {
+    document.querySelector(".footer").style.color = "gray";
+    if (count > 0) {
+      circleUp.classList.remove("down");
+      circleDown.classList.remove("down");
+      button.classList.add("gone");
+      button.innerHTML = "";
+      clearInterval(timer);
+      timer = setInterval(() => {
+        button.style.display = "none";
+      }, 1000);
+
+      audio.pause();
+      clearInterval(audioTimer);
+      text.innerHTML =
+        "Have a Great Day!<br>May your dreams come true.<br><br><div class='note'>~Your loving friend</div>";
+    }
+    if (count === 0) {
+      count++;
+
+      circleUp.classList.add("down");
+      circleDown.classList.add("down");
+      html.classList.add("Clicked");
+      button.innerHTML = "Stop Audio";
+      text.innerHTML = `Happy Birthday ${promp}`;
+      let promise = audio.play();
+
+      audioTimer = setInterval(() => {
+        promise = audio.play();
+      }, 100);
+      timer = setInterval(setColor, 1000);
+    }
+  }
+});
+
+function setColor() {
+  // let randomBg =
+  //     "rgb(" +
+  //     Math.floor(Math.random() * 250) +
+  //     "," +
+  //     Math.floor(Math.random() * 250) +
+  //     "," +
+  //     Math.floor(Math.random() * 250) +
+  //     ")";
+  // linear-gradient(90deg, rgb(221, 32, 32), rgb(67, 79, 252));
+  let randomBg =
+    "linear-gradient(90deg," +
+    "rgb(" +
+    Math.floor(Math.random() * 250) +
+    "," +
+    Math.floor(Math.random() * 250) +
+    "," +
+    Math.floor(Math.random() * 250) +
+    ")," +
+    "rgb(" +
+    Math.floor(Math.random() * 250) +
+    "," +
+    Math.floor(Math.random() * 250) +
+    "," +
+    Math.floor(Math.random() * 250) +
+    "))";
+
+  text.style.color =
+    "rgb(" +
+    Math.floor(Math.random() * 250) +
+    "," +
+    Math.floor(Math.random() * 250) +
+    "," +
+    Math.floor(Math.random() * 250) +
+    ")";
+  html.style.background = randomBg;
+}
